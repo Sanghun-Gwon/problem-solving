42dd3d37c9d274c7c7dd243b104ae4245cd0af2e
sanghun_gwon <sanghun_gwon@naver.com>
2024-10-16 23:07:41 +0900

    two sum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import leetcode.easy.LongestCommonPrefix;
+import leetcode.easy.TwoSum;
+import leetcode.easy.ValidParentheses;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        System.out.println("1. two sum");
+        TwoSum twoSum = new TwoSum();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println("result : " + Arrays.toString(twoSum.twoSum(nums, target)));
 
-        String[] strs = {"flower", "flow", "flight"};
-        System.out.println(longestCommonPrefix.longestCommonPrefix(strs));
+        System.out.println("14. longest common prefix");
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String[] strs = {"flower", "flow","flight"};
+        System.out.println("result : " + longestCommonPrefix.longestCommonPrefix(strs));
+
+
+        System.out.println("20. valid parentheses");
+        ValidParentheses validParentheses = new ValidParentheses();
+        String s = "()[]{}";
+        System.out.println("result : " + validParentheses.isValid(s));
+
     }
 }
--- src/leetcode/easy/LongestCommonPrefix.java
+++ src/leetcode/easy/LongestCommonPrefix.java
@@ -3,21 +3,20 @@ package leetcode.easy;
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length==0) return "";
-        if(strs.length==1) return strs[0];
 
-        StringBuffer prefix = new StringBuffer();
+        StringBuffer sb = new StringBuffer();
 
-        for (int i = 0; i < strs.length; i++) {
+        for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
 
             for(String str:strs) {
                 if(str.length() <= i || c!=str.charAt(i)) {
-                    return prefix.toString();
+                    return sb.toString();
                 }
             }
 
-            prefix.append(c);
+            sb.append(c);
         }
-        return prefix.toString();
+        return sb.toString();
     }
 }
